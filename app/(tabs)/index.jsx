003fe7aea30c0409fe7c416b298b694e4a55805f
Muhammad Saaim <85861944+MSaaim@users.noreply.github.com>
2025-11-26 18:02:35 +0500

    init

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,12 @@
+import { Hospital } from 'lucide-react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <Hospital size={48} color="black" />
+    </SafeAreaView>
+  )
+}
+
+export default index
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -14,10 +14,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+       <Stack
+            screenOptions={{
+              headerShown: false, // hide default header
+              contentStyle: { backgroundColor: "white" }, // force white background
+            }}
+          />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
